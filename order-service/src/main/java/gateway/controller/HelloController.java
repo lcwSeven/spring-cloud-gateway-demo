0e936c39f8f559ba64f5322e7f6cfd49535ae9ef
lcwSeven <liucaiwenseven@163.com>
2021-07-03 16:42:27 +0800

    import:产品微服务，订单微服务搭建

---
+++ order-service/src/main/java/gateway/controller/HelloController.java
@@ -0,0 +1,17 @@
+package gateway.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liucaiwen
+ * @date 2021/7/3
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/order/{id}")
+    public String queryOrder(@PathVariable("id")String id){
+        return "获取订单:"+id;
+    }
+}
